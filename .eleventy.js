75fef3f6e395d0223fe0e9bf5b13781b9f0b121a
Kiselev Andrey <omega-faworit@yandex.ru>
2025-01-05 19:31:33 +0300

    add manifest

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,35 @@
 import { execSync } from 'node:child_process';
 import { writeFile } from 'node:fs/promises';
+import Image from '@11ty/eleventy-img';
+
+await Image('./favicon-inkscape.svg', {
+  widths: [32, 128, 192, 256, 512],
+  outputDir: 'src/assets/generated',
+  formats: ['png'],
+  useCache: false,
+  filenameFormat: (id, src, width, format) => `icon-${width}.${format}`,
+});
+await Image('./favicon-inkscape.svg', {
+  widths: [32, 128, 192, 256, 512],
+  outputDir: 'src/assets/generated',
+  formats: ['png'],
+  formatHooks: {
+    png(sharp) {
+      const roundedCorners = Buffer.from(
+        `<svg width="${this.width}" height="${this.width}">
+        <circle cx="${this.width / 2}" cy="${this.width / 2}" r="${
+          this.width / 2
+        }" fill="white" />
+      </svg>`
+      );
+      return sharp.composite([
+        { input: roundedCorners, top: 0, left: 0, blend: 'dest-in' },
+      ]);
+    },
+  },
+  useCache: false,
+  filenameFormat: (id, src, width, format) => `icon-rounded-${width}.${format}`,
+});
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default (eleventyConfig) => {
@@ -23,7 +53,8 @@ export default (eleventyConfig) => {
   eleventyConfig.ignores.add('./src/templates/*.njk');
   //#endregion
 
-  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/assets/**/*.png');
+  eleventyConfig.addWatchTarget('./src/assets');
 
   return {
     dir: {
